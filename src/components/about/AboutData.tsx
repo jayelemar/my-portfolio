815a12f474f0ba6472b5f78a0193cb29f9b56b1c
Jay Termulo <jetermulo@gmail.com>
2024-02-10 03:38:22 +0800

    feat: about section

---
+++ src/components/about/AboutData.tsx
@@ -0,0 +1,136 @@
+import { HomeIcon, MailIcon, PhoneCall, User2 } from "lucide-react";
+import { SiCss3, SiFigma, SiGit, SiGithub, SiHtml5, SiJavascript, SiReact, SiReactquery, SiRedux, SiTailwindcss, SiTypescript, SiVisualstudiocode, SiExpress, SiFirebase, SiMongodb, SiPostman  } from "react-icons/si";
+import { FaSass } from "react-icons/fa";
+import { TbBrandNextjs } from "react-icons/tb";
+import { FaNodeJs } from "react-icons/fa";
+
+
+export type infoProps = {
+  icon: React.ReactElement,
+  text: string,
+}
+
+export const info: infoProps[] = [
+  {
+    icon: <User2 size={23} />,
+    text: "Jay Elemar Termulo",
+  },
+  {
+    icon: <PhoneCall size={20} />,
+    text: "+63 969 256 1601",
+  },
+  {
+    icon: <MailIcon size={20} />,
+    text: "jetermulo@gmail.com",
+  },
+  {
+    icon: <HomeIcon size={20} />,
+    text: "Bulacan, Philippines",
+  }
+]
+
+export type DataProps = {
+  name: string,
+  imgPath: React.ReactElement,
+  color?: string
+}
+
+export type SkillSetProps = {
+  title: string,
+  data: DataProps[],
+}
+
+export const skillSet: SkillSetProps[]  = [
+  {
+    title: 'Frontend',
+    data: [
+      {
+        name: 'Javascript',
+        imgPath: <SiJavascript size={60} />, 
+        color: 'text-[#efd81d]'
+      },
+      {
+        name: 'React',
+        imgPath: <SiReact size={60} />,
+        color: 'text-[#00d1f7]'
+      },
+      {
+        name: 'NextJS 13',
+        imgPath: <TbBrandNextjs size={60} />,
+        color: 'text-black'
+      },
+      {
+        name: 'Typescript',
+        imgPath: <SiTypescript size={60} />,
+        color: 'text-[#0076c6]'
+      },
+
+      {
+        name: 'Sass',
+        imgPath: <FaSass size={60} />,
+        color: 'text-[#c66394]'
+      },
+      {
+        name: 'Tailwind',
+        imgPath: <SiTailwindcss size={60} />,
+        color: 'text-[#08b1cf]'
+      },
+
+      {
+        name: 'React Query',
+        imgPath: <SiReactquery size={60} />,
+        color: 'text-[#f73f51]'
+      },
+
+    ]
+  },
+  {
+    title: 'Backend',
+    data: [
+      {
+        name: 'NodeJS',
+        imgPath: <FaNodeJs size={65} />,
+        color: 'text-[#509941]'
+      },
+      {
+        name: 'ExpressJS',
+        imgPath: <SiExpress size={65} />,
+      },
+      {
+        name: 'Firebase',
+        imgPath: <SiFirebase size={60} />,
+        color: 'text-[#f7c52a]'
+      },
+      {
+        name: 'MongoDB',
+        imgPath: <SiMongodb size={65} />,
+        color: 'text-[#50ae3e]'
+      },
+
+    ]
+  },
+  {
+    title: 'Others',
+    data: [
+      {
+        name: 'VScode',
+        imgPath: <SiVisualstudiocode size={60} />,
+        color: 'text-[#36aaf2]'
+      },
+      {
+        name: 'Git',
+        imgPath: <SiGit size={70} />,
+        color: 'text-[#e84d31]'
+      },
+      {
+        name: 'Github',
+        imgPath: <SiGithub size={60} />
+      },
+      {
+        name: 'Postman',
+        imgPath: <SiPostman size={60} />,
+        color: 'text-[#f76935]'
+      },
+    ]
+  },
+]
---
+++ src/components/about/AboutSkillList.tsx
@@ -0,0 +1,67 @@
+'use client';
+
+import Image from "next/image";
+import { FC,  } from "react";
+
+import { CarouselContent, Carousel, CarouselItem, CarouselPrevious, CarouselNext } from "../ui/carousel";
+import AutoScroll from 'embla-carousel-auto-scroll'
+
+import AboutSkillFrontend from "./AboutSkillFrontend";
+import AboutSkillBackend from "./AboutSkillBackend";
+import AboutSkillOthers from "./AboutSkillOthers";
+import { SkillSetProps } from "./AboutData";
+
+
+
+export type AboutSkillListProps = {
+  skillSet: SkillSetProps[],
+  getData: (arr: SkillSetProps[], title: string) => SkillSetProps | undefined;
+}
+
+const AboutSkillList:FC<AboutSkillListProps> = ({ skillSet, getData }) => {
+
+
+  return (
+    <div className="">
+    <Carousel
+      opts={{ align: 'start', loop: true, containScroll: "keepSnaps"}}
+      plugins={[
+        AutoScroll({ 
+          speed: 1,
+          startDelay: 100,
+          playOnInit: true,
+          stopOnInteraction: false,
+          stopOnMouseEnter: true,
+        }),
+      ]}
+      className="w-[500px]"
+      
+    >
+      <CarouselContent className="">
+
+        <CarouselItem>
+          <AboutSkillFrontend skillSet={skillSet} getData={getData}  />
+        </CarouselItem>
+        <CarouselItem>
+          <AboutSkillBackend skillSet={skillSet} getData={getData} />
+        </CarouselItem>
+        <CarouselItem>
+          <AboutSkillOthers skillSet={skillSet} getData={getData} />
+        </CarouselItem>
+        <CarouselItem>
+          <AboutSkillFrontend skillSet={skillSet} getData={getData} />
+        </CarouselItem>
+        <CarouselItem>
+          <AboutSkillBackend skillSet={skillSet} getData={getData} />
+        </CarouselItem>
+        <CarouselItem>
+          <AboutSkillOthers skillSet={skillSet} getData={getData} />
+        </CarouselItem>
+  
+      </CarouselContent>
+    </Carousel>
+    </div>
+  )
+}
+
+export default AboutSkillList
