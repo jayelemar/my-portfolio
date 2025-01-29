4179a7a6bff16795dbf524cb8ffd5c8d60bdbc33
Jay Termulo <jetermulo@gmail.com>
2025-01-30 02:32:01 +0800

    bugfix: about section information layout shift

--- src/components/about/AboutInfo.tsx
+++ src/components/about/AboutInfo.tsx
@@ -1,38 +1,44 @@
-import React from 'react'
+import React from "react";
 import { info } from "./AboutData";
-import { GraduationCap } from 'lucide-react';
+import { GraduationCap } from "lucide-react";
+import { motion } from "framer-motion";
+import { fadeIn } from "@/lib/variant";
 
-import { motion } from 'framer-motion'
-import { fadeIn } from '@/lib/variant';
+interface AboutInfoProps {
+  containerStyles: string;
+}
 
-const AboutInfo = () => {
+const AboutInfo = ({ containerStyles }: AboutInfoProps) => {
   return (
-    <motion.div 
-      variants={fadeIn('up', 0.2)}
-      initial='hidden'
-      whileInView={'show'}
-      viewport={{once:false, amount: 0.2}}
+    <motion.div
+      variants={fadeIn("up", 0.2)}
+      initial="hidden"
+      whileInView={"show"}
+      viewport={{ once: false, amount: 0.2 }}
+      className={containerStyles}
     >
       {info.map((item, index) => {
         return (
-          <div 
-            className="flex items-center gap-x-4 m-6 pl-4 mx-auto xl:mx-0 text-left text-balance" key={index}>
+          <div
+            className="m-6 mx-auto flex items-center gap-x-4 text-balance pl-4 text-left xl:mx-0"
+            key={index}
+          >
             <div className="text-primary">{item.icon}</div>
             <div className="">{item.text}</div>
           </div>
-        )
+        );
       })}
-      <div
-        className=" items-center gap-x-4 m-6 pl-4 mx-auto xl:mx-0 text-left text-balance hidden md:flex"
-      >
-        <div className="text-primary"><GraduationCap size={25} /></div>
-        <div className="text-left">
-          <p className="">Bachelor of Science in</p>
-          <p className="">Electronics Engineering</p>
+      <div className=" m-6 mx-auto hidden items-center gap-x-4 text-balance pl-4 text-left md:flex xl:mx-0">
+        <div className="text-primary">
+          <GraduationCap size={25} />
+        </div>
+        <div className="flex flex-col gap-x-2 text-left lg:flex-row">
+          <span className="">Bachelor of Science in</span>
+          <span className="">Electronics Engineering</span>
         </div>
       </div>
     </motion.div>
-  )
-}
+  );
+};
 
-export default AboutInfo
+export default AboutInfo;
